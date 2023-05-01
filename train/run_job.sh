83c044fe499dbe8f56af4b1756c848bb08f3563c
Hao Li <haoli@haos-mbp.mynetworksettings.com>
2023-05-01 13:58:42 -0400

    remove pip

--- train/run_job.sh
+++ train/run_job.sh
@@ -3,10 +3,11 @@
 current_dir=$(cd $(dirname $0); pwd)
 echo "current_dir $current_dir"
 
-which pip
-python -m pip install -r $current_dir/requirements.txt
+# which pip
+# python -m pip install -r $current_dir/requirements.txt
+
 python $current_dir/main.py --num-workers 10
 
 run_current_dir=$(pwd)
 echo "clean $run_current_dir"
-# rm -fr $run_current_dir/cml_proj2
+rm -fr $run_current_dir/cml_proj2
