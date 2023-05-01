8848900ea3dc02b3f73d7dfde80721eb2c9e0af6
Hao Li <haoli@haos-mbp.mynetworksettings.com>
2023-05-01 12:43:39 -0400

    update run_job.sh

--- train/run_job.sh
+++ train/run_job.sh
@@ -3,7 +3,8 @@
 current_dir=$(cd $(dirname $0); pwd)
 echo "current_dir $current_dir"
 
-pip install -r $current_dir/requirements.txt
+which pip
+python -m pip install -r $current_dir/requirements.txt
 python $current_dir/main.py --num-workers 10
 
 run_current_dir=$(pwd)
